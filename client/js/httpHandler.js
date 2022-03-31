25fa9a1ac8728edba6f60aaf8307866c41c10c34
David Clay <dclay1983@gmail.com>
2022-03-31 13:57:03 -0700

    init

---
+++ client/js/httpHandler.js
@@ -0,0 +1,49 @@
+(function() {
+
+  const serverUrl = 'http://127.0.0.1:3000';
+
+
+  //TODO: build the swim command fetcher here
+
+
+  /////////////////////////////////////////////////////////////////////
+  // The ajax file uploader is provided for your convenience!
+  // Note: remember to fix the URL below.
+  /////////////////////////////////////////////////////////////////////
+
+  const ajaxFileUpload = (file) => {
+    var formData = new FormData();
+    formData.append('file', file);
+    $.ajax({
+      type: 'POST',
+      data: formData,
+      url: 'FILL_ME_IN',
+      cache: false,
+      contentType: false,
+      processData: false,
+      success: () => {
+        // reload the page
+        window.location = window.location.href;
+      }
+    });
+  };
+
+  $('form').on('submit', function(e) {
+    e.preventDefault();
+
+    var form = $('form .file')[0];
+    if (form.files.length === 0) {
+      console.log('No file selected!');
+      return;
+    }
+
+    var file = form.files[0];
+    if (file.type !== 'image/jpeg') {
+      console.log('Not a jpg file!');
+      return;
+    }
+
+    ajaxFileUpload(file);
+  });
+
+})();
